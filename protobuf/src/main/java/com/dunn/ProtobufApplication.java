faf349ec22ffc947303dbb7768210610a1f5f385
Dunn <v_dunn@yeahka.com>
2022-06-21 17:59:21 +0800

    protobuf

---
+++ protobuf/src/main/java/com/dunn/ProtobufApplication.java
@@ -0,0 +1,16 @@
+package com.dunn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Dunn
+ */
+@SpringBootApplication
+public class ProtobufApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtobufApplication.class, args);
+    }
+
+}
